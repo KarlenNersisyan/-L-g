81a6d672c0e136ef60b5d187ebc5c7e5435aaf91
KarlNersisyan <karlennersisyan814@gmail.com>
2021-07-28 21:57:53 +0400

    feat(App): Add cssBaseline(normalize)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div>
-      <h1>React-Blog</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Blog from './components/Blog';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <CssBaseline /> 
+        {/* You might be familiar with normalize.css, a collection of HTML element and attribute style-normalizations. */}
+        <Router>
+          <Blog />
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
