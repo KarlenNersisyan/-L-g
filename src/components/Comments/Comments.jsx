e93e20157824e8dd83a5581c750f5dd9a84d6f50
KarlNersisyan <karlennersisyan814@gmail.com>
2021-07-29 01:04:27 +0400

    feat(Comments): Editting only for author of comment

---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+
+import SingleComment from '../SingleComment/SingleComment';
+
+export default class Comments extends Component {
+  render() {
+    const {
+      comments,
+      users,
+      handleEditComment,
+      handleDeleteComment,
+      currentUser,
+    } = this.props;
+
+    return (
+      <Grid container justify="center">
+        {comments.map(({ userId, title, content, date, id, postId }) => (
+          <Grid key={id} item xs={12} sm={9}>
+            <SingleComment
+              userId={userId}
+              title={title}
+              content={content}
+              date={date}
+              id={id}
+              users={users}
+              postId={postId}
+              currentUser={currentUser}
+              handleEditComment={handleEditComment.bind(this, {
+                title,
+                content,
+                id,
+                date,
+                userId,
+                postId,
+              })}
+              handleDeleteComment={handleDeleteComment.bind(this, {
+                title,
+                content,
+                id,
+                date,
+                userId,
+                postId,
+              })}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
+}
