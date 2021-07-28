eb4133b58c24e2057e5eb73c0dccfa6c2a8325a9
KarlNersisyan <karlennersisyan814@gmail.com>
2021-07-29 00:33:07 +0400

    fix(ProtectedRoute): path for '/blog-byNersisyan/'

---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        rest.isLoggedIn ? (
+          <Component {...props} {...rest} postId={props.match.params.postId} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/blog-byNersisyan/auth',
+              state: { referrer: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
